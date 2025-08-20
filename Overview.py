a48a4c405a494a42c8694723493fac6a0a13eba6
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-08-21 05:15:56 +0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -24,8 +24,8 @@ st.write(''' Seperti namanya, kopi Specialty adalah sebuah penilaian atau pengkl
 
 col1,col2 = st.columns(2)
 with col1:
-    st.markdown(''' **Awal Mula Penilaian: Dari Tampilan Fisik ke Rasa**''')
-st.write('''Pada awalnya, penilaian kopi didasarkan pada tampilan fisik biji. Kopi dinilai berdasarkan ukuran, bentuk, dan warna biji,
+    st.markdown(''' **Awal Mula Penilaian: Dari Tampilan Fisik ke Rasa** \n
+Pada awalnya, penilaian kopi didasarkan pada tampilan fisik biji. Kopi dinilai berdasarkan ukuran, bentuk, dan warna biji,
 serta jumlah cacat fisik yang terlihat. Sistem ini, yang masih digunakan di beberapa pasar komersial hingga kini (seperti standar SNI yang berbasis cacat),
 seringkali tidak mencerminkan kualitas rasa kopi saat diseduh.
 
@@ -35,7 +35,7 @@ terkadang menghasilkan rasa yang luar biasa. Oleh karena itu, muncullah kebutuha
 ''')
 
 with col2:
-    st.markdown('''**Era "Spesialti Kopi" dan "Third Wave"**
+    st.markdown('''**Era "Spesialti Kopi" dan "Third Wave"** \n
 Istilah "Specialty Coffee" pertama kali diperkenalkan oleh Erna Knutsen dari Knutsen Coffee Ltd. dalam sebuah artikel pada tahun 1974.
 Ia mendefinisikan kopi spesialti sebagai kopi yang memiliki profil rasa khusus dan berasal dari biji yang ditanam di iklim mikro tertentu.
 
@@ -57,6 +57,7 @@ Dimana setelah setelah mengetahui karakteristik karakteristik diatas lalu data t
 Dalam Project ini algoritma yang digunakan adalah menggunakan algoritma K-Means,dimana Algoritma K-Means sendiri sering digunakan dalam project-project berbasis unsupervised learning,dimana algoritma ini memiliki keuntungan yang diantaranya adalah :
 - Cepat dan efisien, terutama pada dataset yang besar.
 - Mudah dipahami dan diimplementasikan.''')
+
 
 
 
