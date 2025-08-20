49ee82d0d5db40c11f16442b08cfc24e2ff2fa66
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-08-21 05:26:42 +0700

    Update Overview.py

--- Overview.py
+++ Overview.py
@@ -46,20 +46,40 @@ Gerakan ini mendorong para konsumen untuk melihat kopi seperti halnya anggur—s
 - Cara Pemrosesan
 ''')
 
-st.markdown(''' Tujuan melakukan Clustering ini adalah untuk mengidentifikasi karakteristik pelanggan dimana dalam kasus ini adalah menggunakan Karakteristik-karakteristik,seperti :
-- Recency
-- Frequency
-- Monetary
-- Category
+col3,col4 = st.columns(2)
+with col3:
+    st.markdown(''' **Pembentukan SCA dan Standarisasi Penilaian** \n
 
-Dimana setelah setelah mengetahui karakteristik karakteristik diatas lalu data tersebut diolah untuk melakukan pemetaan cluster dari data yang diperoleh tersebut,lalu setelah dimasukkan mesin akan mengolah data tersebut guna memahami dengan karakteristik sebagai berikut termasuk kedalam cluster yang mana.
+Pada tahun 1982, didirikanlah Specialty Coffee Association of America (SCAA). Organisasi ini menjadi tonggak penting dalam standarisasi penilaian kopi spesialti.
+SCAA, yang kemudian merger dengan Specialty Coffee Association of Europe (SCAE) pada tahun 2017 menjadi Specialty Coffee Association (SCA),
+mengembangkan sistem penilaian yang dikenal sebagai SCA Cupping Protocol atau Q-Grading System.
 
-Dalam Project ini algoritma yang digunakan adalah menggunakan algoritma K-Means,dimana Algoritma K-Means sendiri sering digunakan dalam project-project berbasis unsupervised learning,dimana algoritma ini memiliki keuntungan yang diantaranya adalah :
-- Cepat dan efisien, terutama pada dataset yang besar.
-- Mudah dipahami dan diimplementasikan.''')
+Sistem ini, yang masih menjadi acuan global hingga kini, memiliki karakteristik utama sebagai berikut:
+1. Sistem 100 Poin: Kopi dinilai pada skala 100 poin, di mana kopi dengan skor 80 atau lebih dianggap sebagai kopi spesialti.
+2. Penilaian Sensorik: Penilaian tidak lagi hanya berdasarkan cacat fisik, tetapi berfokus pada atribut rasa dan aroma, seperti:
+- Fragrance/Aroma (Aroma kering dan basah)
+- Flavor (Rasa)
+- Aftertaste (Sisa Rasa setelah ditelan)
+- Acidity (Keasaman)
+- Body (Kekentalan)
+- Balance (Keseimbangan)
+- Clean Cup (Kebersihan Rasa)
+- Uniformity (Keseragaman Rasa)
+- Sweetness (Rasa Manis)
+- Overall (kesan keseluruhan)
 
+3. Pelatihan Profesional: SCA memperkenalkan program sertifikasi untuk para penilai kopi, yang dikenal sebagai Q Grader.
+Mereka dilatih untuk mencicipi dan menilai kopi secara objektif dan konsisten menggunakan standar yang sama di seluruh dunia.
+''')
 
+with col4:
+    st.markdown('''**Era "Spesialti Kopi" dan "Third Wave"** \n
+Istilah "Specialty Coffee" pertama kali diperkenalkan oleh Erna Knutsen dari Knutsen Coffee Ltd. dalam sebuah artikel pada tahun 1974.
+Ia mendefinisikan kopi spesialti sebagai kopi yang memiliki profil rasa khusus dan berasal dari biji yang ditanam di iklim mikro tertentu.
 
-
-
-
+st.Konsep ini kemudian berkembang pesat, sejalan dengan munculnya gerakan "Third Wave of Coffee" pada akhir abad ke-20.
+Gerakan ini mendorong para konsumen untuk melihat kopi seperti halnya anggur—sebagai produk pertanian yang memiliki karakteristik unik berdasarkan :
+- Varietas
+- Asal
+- Cara Pemrosesan
+''')
