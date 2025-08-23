8e8dd15ec885a8f20cb542d37864fc19fa713390
Bagus Rahma AC <46398272+isNothingSpecial@users.noreply.github.com>
2025-08-23 14:48:50 +0700

    Update Literatur.py

--- pages/Literatur.py
+++ pages/Literatur.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import importlib
 
 def show_literatur_page():
     st.header("📚 Literatur & Informasi")
